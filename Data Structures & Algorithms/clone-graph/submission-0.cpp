9899446a0d1e4de8c8681e2e7ebf4d612fed040e
prachi941 <prachijain.pj30@gmail.com>
2026-07-15 00:22:07 +0530

    Add: clone-graph - submission-0

---
+++ Data Structures & Algorithms/clone-graph/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    unordered_map<Node*, Node*> mp;
+
+    Node* cloneGraph(Node* node) {
+
+        if(node == NULL) {
+            return NULL;
+
+        }
+
+        if(mp.find(node) != mp.end()) {
+            return mp[node];
+        }
+
+        Node* clone = new Node(node->val);
+
+        mp[node] = clone;
+
+        for(Node* neighbor : node->neighbors) {
+            clone->neighbors.push_back(cloneGraph(neighbor));
+        }
+        
+        return clone;
+
+    }
+};
