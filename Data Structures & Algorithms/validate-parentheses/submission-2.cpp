25fe70fb14ebab6f8f6a8c07ac17ccf7196507fe
prachi941 <prachijain.pj30@gmail.com>
2026-08-05 22:03:07 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+
+        unordered_map<char, char> mp = {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        for(char ch : s) {
+
+            if(ch == '(' || ch == '{' || ch == '[') {
+                st.push(ch);
+            }
+
+            else{
+
+                if(st.empty()) 
+                 return false;
+
+                if(st.top() != mp[ch])
+                 return false;
+
+                st.pop();
+            }
+
+        }
+
+        return st.empty();
+        
+    }
+};
